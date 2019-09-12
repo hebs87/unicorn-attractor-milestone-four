c58b37a29798004fe342ea71ff98b6ef9c47e200
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-12 10:00:36 +0000

    Created Profile model to enable user profile image upload

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    Profile model that has a one to one relationship with the user
+    This ensures only one profile can be uploaded per user
+    Uses CASCADE to ensure the profile is deleted if the user is deleted
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
