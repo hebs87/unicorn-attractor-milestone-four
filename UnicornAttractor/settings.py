2ba368aabebfe70dcb16d709180fbd619a3ee127
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-12 10:20:51 +0000

    Added media root config to settings.py for media uploads

--- UnicornAttractor/settings.py
+++ UnicornAttractor/settings.py
@@ -123,13 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATICFILES_LOCATION = "static"
-# STATICFILES_STORAGE = "custom_storages.StaticStorage" # enabled only on local
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
