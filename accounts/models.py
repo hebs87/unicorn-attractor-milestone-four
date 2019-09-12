dbb7b5d52eb62577a8241a8f403531989c170635
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-12 14:01:16 +0000

    Updated Profile model to resize images before uploading

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -14,6 +15,20 @@ class Profile(models.Model):
     
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+
+    def save(self):
+        '''
+        Reduce the image size before saving in the local storage
+        '''
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
 def create_profile(sender, instance, created, **kwargs):
