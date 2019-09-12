8148856cccaa8666f3ac2ebe7fbe07edb82c3af1
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-12 10:58:14 +0000

    Imported signals into accounts apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+
+    def ready(self):
+        '''
+        Import signals into the apps
+        '''
+        import accounts.signals
