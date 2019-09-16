fca9ec53387206b1fb60ed5c320d0e0ddbb5120b
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-16 10:32:41 +0000

    Created Ticket model in tickets app and added type and status choices

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Ticket(models.Model):
+    '''
+    Allows users to log bug or feature tickets
+    Uses CASCADE to remove ticket from user's list when deleted
+    Auto-adds current date to created_date/edited_date
+    '''
+    # Choices for ticket type and status
+    TICKET_TYPE_CHOICES = (
+        ("Bug", "Bug"),
+        ("Feature", "Feature"),
+    )
+    TICKET_STATUS_CHOICES = (
+        ("Open","Open"),
+        ("In Progress", "In Progress"),
+        ("Closed", "Closed"),
+    )
