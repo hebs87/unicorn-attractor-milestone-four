b570b1f68d9cd7884a9f696a1861db9ff27c6e71
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-16 10:57:06 +0000

    Added fields to Ticket model in tickets app

--- tickets/models.py
+++ tickets/models.py
@@ -19,3 +19,39 @@ class Ticket(models.Model):
         ("In Progress", "In Progress"),
         ("Closed", "Closed"),
     )
+    
+    user = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.CASCADE)
+    created_date = models.DateTimeField(
+        blank=False,
+        null=False,
+        auto_now_add=True)
+    ticket_type = models.CharField(
+        max_length=7,
+        choices=TICKET_TYPE_CHOICES)
+    ticket_status = models.CharField(
+        max_length=11,
+        choices=TICKET_STATUS_CHOICES)
+    title = models.CharField(
+        max_length=100,
+        blank=False)
+    description = models.TextField(
+        max_length=2000,
+        blank=False)
+    views = models.IntegerField(
+        default=0)
+    upvotes = models.IntegerField(
+        default=0)
+    edited_date = models.DateTimeField(
+        blank=False,
+        null=False,
+        default=timezone.now)
+    total_donations = models.IntegerField(
+        default=0)
+
+
+    def __str__(self):
+        return "#{0} [{1}] - {2}".format(
+            self.ticket_type, self.ticket_status, self.title)
