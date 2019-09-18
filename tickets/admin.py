b93d0649834a7e6c7918ba022804f46b7ba9e131
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-18 11:40:49 +0000

    Registered TicketType and TicketStatus models to admin site

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Ticket, Comment, Upvote
+from .models import TicketType, TicketStatus, Ticket, Comment, Upvote
 
 # Register your models here.
+admin.site.register(TicketType)
+admin.site.register(TicketStatus)
 admin.site.register(Ticket)
 admin.site.register(Comment)
 admin.site.register(Upvote)
