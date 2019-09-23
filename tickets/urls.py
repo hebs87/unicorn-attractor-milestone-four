10d90f30f0f1645fc53468b8373c89e7a27dce27
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-23 10:59:13 +0000

    Added new_feature_ticket url to tickets urls.py

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import view_all_tickets, new_bug_ticket
+from .views import view_all_tickets, new_bug_ticket, new_feature_ticket
 
 
 urlpatterns = [
     url(r'^$', view_all_tickets, name="view_all_tickets"),
     url(r"^new/bug$", new_bug_ticket, name="new_bug_ticket"),
+    url(r"^new/feature$", new_feature_ticket, name="new_feature_ticket"),
 ]
