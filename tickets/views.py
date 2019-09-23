60fb8255ef8e752028ce69d331ed45de188b5f05
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-23 14:37:10 +0000

    Added functionality to save feature ticket once payment submitted successfully

--- tickets/views.py
+++ tickets/views.py
@@ -108,15 +108,44 @@ def new_feature_ticket(request):
                 # Display error message if card is declined
                 messages.error(request, "Your card was declined!")
 
-            
-
+            # If payment is successful
+            if customer.paid:
+                feature_form.instance.user = request.user
+                feature_form.instance.ticket_type_id = 2
+                feature_form.instance.total_donations = donation_amount
+                # Add the donation to the user's total_donated amount
+                # Get the user's current donations...
+                user_total_donated = Profile.objects.values_list(
+                    "total_donated", flat=True).get(user_id=request.user.id)
+                # Add it to the donation amount
+                new_total_donated = user_total_donated + donation_amount
+                # Push the new amount to the user's total_donated amount
+                Profile.objects.filter(user_id=request.user.id).update(
+                    total_donated=new_total_donated)
+                # Update the ticket's status to In Progress if user donates
+                # the goal amount for the feature to be implemented
+                if donation_amount == int(100):
+                    feature_form.instance.ticket_status = 2
+                else:
+                    # If goal amount not reached, update status to Open
+                    feature_form.instance.ticket_status = 1
+                new_feature = feature_form.save()
+                messages.success(request, f"Thanks for submitting a Feature Request!")
+                return redirect(view_all_tickets)
+            else:
+                messages.error(request, "Unable to take payment")
+        # If feature_form or donation_form aren't valid
+        else:
+            messages.error(request, f"We were unable to take a payment with \
+                           that card. Please try again.")
     else:
         feature_form = TicketForm()
         donation_form = DonationForm()
 
     args = {
         "feature_form": feature_form,
-        "donation_form": donation_form
+        "donation_form": donation_form,
+        "publishable": settings.STRIPE_PUBLISHABLE
     }
 
     return render(request, "new_feature.html", args)
