d49b2ba4d939c1843e12d21fdc1f0bccbc5e7750
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-09-28 15:10:52 +0000

    Added delete_ticket url to tickets urls.py file

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import view_all_tickets, new_bug_ticket, new_feature_ticket, view_single_ticket
+from .views import view_all_tickets, new_bug_ticket, new_feature_ticket, view_single_ticket, delete_ticket
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r"^new/bug$", new_bug_ticket, name="new_bug_ticket"),
     url(r"^new/feature$", new_feature_ticket, name="new_feature_ticket"),
     url(r"^(?P<pk>\d+)$", view_single_ticket, name="view_single_ticket"),
+    url(r"^delete(?P<pk>\d+)$", delete_ticket, name="delete_ticket"),
 ]
