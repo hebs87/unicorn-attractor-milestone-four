ed26bf923d30e8a1297b5b0259fffaeb5c0fffa0
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-08 10:15:04 +0000

    Added downvote url to tickets urls.py and amended downvote btn display

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import view_all_tickets, new_bug_ticket, new_feature_ticket, view_single_ticket, delete_ticket, edit_ticket, upvote
+from .views import view_all_tickets, new_bug_ticket, new_feature_ticket, view_single_ticket, delete_ticket, edit_ticket, upvote, downvote
 
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r"^edit/(?P<pk>\d+)$", edit_ticket, name="edit_ticket"),
     url(r"^delete/(?P<pk>\d+)$", delete_ticket, name="delete_ticket"),
     url(r"^upvote/(?P<pk>\d+)$", upvote, name="upvote"),
+    url(r"^downvote/(?P<pk>\d+)$", downvote, name="downvote"),
 ]
