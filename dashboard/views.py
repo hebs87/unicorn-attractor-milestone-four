da467f2494bdc0fb199a3318700d13463c392921
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-12 11:11:56 +0000

    Added total ticket count for each ticket type to dashboard views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,12 +10,20 @@ def dashboard_stats(request):
     # Filter tickets by ticket_type and store in variable
     all_bugs = Ticket.objects.filter(ticket_type=1)
     all_features = Ticket.objects.filter(ticket_type=2)
-    
+
+    # Total count of each ticket type
+    total_bugs = all_bugs.count()
+    total_features = all_features.count()
+
     # TOP 5 Bugs and Features based on upvotes
     top_five_bugs = all_bugs.order_by("-upvotes")[:5]
     top_five_features = all_features.order_by("-upvotes")[:5]
 
+    
+
     args = {
+        'total_bugs': total_bugs,
+        'total_features': total_features,
         'top_five_bugs': top_five_bugs,
         'top_five_features': top_five_features,
     }
