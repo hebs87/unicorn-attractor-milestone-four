c0b054039c89e9ab324868cb7281be98b88780d0
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-12 10:44:53 +0000

    Created basic dashboard view to render dashboard.html

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def dashboard_stats(request):
+    '''
+    Get objects needed for dashboard stats and display on dashboard.html
+    '''
+    return render(request, "dashboard.html")
