4a672a7abb0353755473afced27d259172343011
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-12 11:08:46 +0000

    Filtered top 5 of each ticket type based on upvotes

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,15 +7,17 @@ def dashboard_stats(request):
     '''
     Get objects needed for dashboard stats and display on dashboard.html
     '''
-    # BUGS
+    # Filter tickets by ticket_type and store in variable
     all_bugs = Ticket.objects.filter(ticket_type=1)
-    
-    # FEATURES
     all_features = Ticket.objects.filter(ticket_type=2)
     
+    # TOP 5 Bugs and Features based on upvotes
+    top_five_bugs = all_bugs.order_by("-upvotes")[:5]
+    top_five_features = all_features.order_by("-upvotes")[:5]
+
     args = {
-        'all_bugs': all_bugs,
-        'all_features': all_features,
+        'top_five_bugs': top_five_bugs,
+        'top_five_features': top_five_features,
     }
     
     return render(request, "dashboard.html", args)
