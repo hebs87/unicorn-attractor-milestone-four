be82c34545668253c5435c33c4e1982a9344d42b
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-12 11:02:34 +0000

    Filtered tickets by ticket_type in views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,4 +7,15 @@ def dashboard_stats(request):
     '''
     Get objects needed for dashboard stats and display on dashboard.html
     '''
-    return render(request, "dashboard.html")
+    # BUGS
+    all_bugs = Ticket.objects.filter(ticket_type=1)
+    
+    # FEATURES
+    all_features = Ticket.objects.filter(ticket_type=2)
+    
+    args = {
+        'all_bugs': all_bugs,
+        'all_features': all_features,
+    }
+    
+    return render(request, "dashboard.html", args)
