f4475d9c75c68dd91c7c1a2985d417cb7dd6a290
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-20 14:40:59 +0000

    Created tests for UserRegistrationForm in accounts test_forms.py

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -3,15 +3,15 @@ from .forms import UserLoginForm, UserRegistrationForm, UserUpdateForm, ProfileU
 
 
 class TestUserLoginForm(TestCase):
-    def test_can_log_in(self):
+    def test_user_can_log_in(self):
         '''
         Tests that the user can login with a username and password
         '''
-        form = UserLoginForm({"username": "User", "password": "Password"})
+        form = UserLoginForm({"username": "TestUser", "password": "Password"})
         self.assertTrue(form.is_valid())
 
 
-    def test_invalid_details_error_message(self):
+    def test_blank_field_error_message(self):
         '''
         Tests that the fields are required for the form to be valid
         '''
@@ -19,3 +19,43 @@ class TestUserLoginForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors["username"],
                          [u"This field is required."])
+
+
+class TestUserRegistrationForm(TestCase):
+    def test_user_can_register(self):
+        '''
+        Tests that the user can register by entering the relevant details
+        '''
+        form = UserRegistrationForm({
+            "username": "TestUser",
+            "email": "user@test.com",
+            "first_name": "FirstName",
+            "last_name": "LastName",
+            "password1": "Password",
+            "password2": "Password"
+        })
+        self.assertTrue(form.is_valid())
+
+
+    def test_blank_field_error_message(self):
+        '''
+        Tests that the fields are required for the form to be valid
+        '''
+        form = UserRegistrationForm({"username": ""})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["username"],
+                         [u"This field is required."])
+
+
+    def test_passwords_must_match_error(self):
+        '''
+        Tests that the correct error is displayed when the passwords
+        do not match
+        '''
+        form = UserRegistrationForm({
+            "password1": "Password1",
+            "password2": "Password2"
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["password2"],
+                         [u"Passwords must match"])
