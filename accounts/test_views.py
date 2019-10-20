25216171e0924984f6d00d0f108c1b9e78678377
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-20 20:29:51 +0000

    Created test for rendering login.html

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -5,8 +5,17 @@ from django.shortcuts import reverse
 class TestViews(TestCase):
     def test_get_home_page(self):
         '''
-        Tests that the index view renders the home page
+        Tests that the index view renders the home template
         '''
         page = self.client.get("/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "index.html")
+
+
+    def test_get_login_page(self):
+        '''
+        Tests that the index view renders the login template
+        '''
+        page = self.client.get("/accounts/login/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "login.html")
