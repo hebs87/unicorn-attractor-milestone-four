50f7d5d78eba6e7ccb892f434858492b9fbe85be
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-20 20:32:56 +0000

    Created test for logout view redirecting to index page

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -14,8 +14,17 @@ class TestViews(TestCase):
 
     def test_get_login_page(self):
         '''
-        Tests that the index view renders the login template
+        Tests that the login view renders the login template
         '''
         page = self.client.get("/accounts/login/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "login.html")
+
+
+    def test_logout_view(self):
+        '''
+        Tests that the logout view redirects to the index template
+        '''
+        page = self.client.get("/accounts/logout/")
+        self.assertEqual(page.status_code, 302)
+        self.client.post(reverse('index'))
