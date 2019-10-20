897f9bd1b1b4c9689e133ee7efb854cb80ce42fa
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-20 21:18:02 +0000

    Created test for CommentForm

--- tickets/test_forms.py
+++ tickets/test_forms.py
@@ -33,3 +33,32 @@ class TestDonationForm(TestCase):
         '''
         form = DonationForm({"donation_amount": 5})
         self.assertTrue(form.is_valid())
+
+
+class TestCommentForm(TestCase):
+    def test_user_can_create_comment_over_five_characters(self):
+        '''
+        Tests that the user can create a comment by filling the CommentForm,
+        as long as the comment is over 5 characters
+        '''
+        form = CommentForm({"description": "Test comment"})
+        self.assertTrue(form.is_valid())
+
+
+    def test_user_cannot_create_comment_under_five_characters(self):
+        '''
+        Tests that the user cannot create a comment by filling the
+        CommentForm, if the comment length is under 5 characters
+        '''
+        form = CommentForm({"description": "Test"})
+        self.assertFalse(form.is_valid())
+
+
+    def test_blank_field_error_message(self):
+        '''
+        Tests that the field is required for the CommentForm to be valid
+        '''
+        form = CommentForm({"description": ""})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["description"],
+                         [u"This field is required."])
