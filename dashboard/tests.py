f5bb9c204413d9a41c6f376637f934b06b985b16
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-20 20:56:44 +0000

    Created test for rendering dashboard.html

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class TestViews(TestCase):
+    def test_get_dashboard_page(self):
+        '''
+        Tests that the dashboard view renders the dashboard template
+        '''
+        page = self.client.get("/dashboard/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "dashboard.html")
