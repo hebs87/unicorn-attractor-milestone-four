b86c375df668fdb055974e712f6e4b7fbdbf89fe
Ubuntu <ubuntu@ip-172-31-16-182.ec2.internal>
2019-10-25 18:30:05 +0000

    Included if-else in settings.py to set debug to true only in test environment

--- UnicornAttractor/settings.py
+++ UnicornAttractor/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+# Set debug to True only in test environment
+if 'C9_HOSTNAME' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,9 +28,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
                  os.environ.get('HOSTNAME')]
